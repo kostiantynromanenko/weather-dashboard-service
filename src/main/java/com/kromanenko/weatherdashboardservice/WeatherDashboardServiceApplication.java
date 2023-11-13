065b413e297e9902f17134e5625cec73c239861f
Konstantin Romanenko <kostiantyn.romanenko@gmail.com>
2023-11-13 15:13:16 +0200

    Initial commit

---
+++ src/main/java/com/kromanenko/weatherdashboardservice/WeatherDashboardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kromanenko.weatherdashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDashboardServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherDashboardServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/com/kromanenko/weatherdashboardservice/WeatherDashboardServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kromanenko.weatherdashboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDashboardServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
